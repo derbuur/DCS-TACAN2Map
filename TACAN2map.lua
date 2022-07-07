f8a0349bbcb025f6ddff2d368aae9c0f93a95d5b
der buur <derbuur@googlemail.com>
2022-07-07 11:15:49 +0200

    Add files via upload
    Initial upload.

---
+++ TACAN2map.lua
@@ -0,0 +1,68 @@
+local myTACANS = mist.getUnitsByAttribute({typeName = 'TACAN_beacon', coalition = "blue"}, 2, false) --, coalition = "blue"
+trigger.action.outText(mist.utils.tableShow(myTACANS,';'), 100, false)
+
+function getTableSize(t)
+    local count = 0
+    for _, __ in pairs(t) do
+        count = count + 1
+    end
+    return count
+end 
+
+
+
+for ke,val in pairs(myTACANS) 
+		do
+		--trigger.action.outText(val, 100, false)
+		--trigger.action.outText(mist.utils.tableShow(Unit.getByName(val):getPoint()), 100, false)
+		local id_table = world.getMarkPanels( )
+
+		local size_id_table = getTableSize(id_table)
+		
+		--local actualTACAN = val:getGroup():getName()
+		local actualTACAN_group = Unit.getByName(val):getGroup():getName()
+		
+		
+		
+		local t=mist.getGroupRoute( actualTACAN_group , true )[1]["task"]["params"]["tasks"]
+
+		for key,value in pairs(t) 
+				do
+					if t[key]["id"] == "WrappedAction" then
+						if t[key]["params"]["action"]["params"]["channel"] 
+							then TCN_channel = t[key]["params"]["action"]["params"]["channel"] 
+							else TCN_channel = "N/A" 
+						end
+					
+						if t[key]["params"]["action"]["params"]["modeChannel"] 
+							then TCN_modeChannel = t[key]["params"]["action"]["params"]["modeChannel"] 
+							else TCN_modeChannel = "N/A" 
+						end
+					
+						if t[key]["params"]["action"]["params"]["callsign"] 
+							then TCN_callsign = t[key]["params"]["action"]["params"]["callsign"] 
+							else TCN_callsign = "N/A" 
+						end
+						msgstr = "TCN: "..TCN_channel..TCN_modeChannel.." "..TCN_callsign
+					else
+						msgstr = "no information"
+					end
+				end
+					
+		
+		
+		
+		
+		trigger.action.textToAll(
+		-1,
+		size_id_table + ke,
+		Unit.getByName(val):getPoint(),
+		{0,0,0, 1},
+		{0.8,0.8,0.8,1},
+		11,
+		true,
+		"©"..msgstr--..v["callsignStr"]
+		)
+		
+		end
+		
